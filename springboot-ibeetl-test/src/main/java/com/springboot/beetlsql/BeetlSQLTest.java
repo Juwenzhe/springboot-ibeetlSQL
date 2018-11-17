180ac58d6e0408474ac363dbd7452cda344e8ffd
juwenzhe <juwenzhe123@163.com>
2018-11-17 21:57:30 +0800

    修改了BeetlSQLTest

--- springboot-ibeetl-test/src/main/java/com/springboot/beetlsql/BeetlSQLTest.java
+++ springboot-ibeetl-test/src/main/java/com/springboot/beetlsql/BeetlSQLTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BeetlSQLTest {
 
 	public static void main(String[] args) {
+		
+		//update Git test
 		SpringApplication.run(BeetlSQLTest.class, args);
 	}
 	
